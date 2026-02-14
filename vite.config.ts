f4b1f5a5591d6f8dd618ae742dafb27563ddb683
朱英豪 <17673298578@163.com>
2026-02-14 23:39:27 +0800

    修复 Service Worker 预缓存路径，添加 /madeWatermelon/ 前缀

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,9 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        modifyURLPrefix: {
+          '': '/madeWatermelon/'
+        },
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/.*/i,
